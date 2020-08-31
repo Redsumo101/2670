1f95fac028c38c15593bc7a0333e8696d15c1bfb
Riley <rileyharper15@gmail.com>
2020-08-31 14:23:43 -0600

    Basic Movement/ Jump and double Jump

---
+++ Test2/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement;
+    public float gravity = 9.81f;
+    public float moveSpeed = 3f;
+    private float jumpForce = 100f;
+    public int jumpCountMax;
+    private int jumpCount;
+    public float jumpForceMax = 100f;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal") * moveSpeed;
+        movement.z = Input.GetAxis("Vertical") * moveSpeed;
+       
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            movement.y = jumpForce;
+            jumpCount -= 1;
+            if (jumpCount == 0)
+            {
+                jumpForce = 0 ;
+            }
+           
+            
+        }
+
+        if (controller.isGrounded)
+        {
+            jumpCount = jumpCountMax;
+            movement.y = 0;
+            jumpForce = jumpForceMax;
+        }
+        else
+        {
+            movement.y -= gravity;
+        }
+        
+       
+       
+        controller.Move(movement* Time.deltaTime);
+    }
+}
