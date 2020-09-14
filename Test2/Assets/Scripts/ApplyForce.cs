9d6129f994eeb5af717aa80485b4b0740a7d7bd6
Riley <rileyharper15@gmail.com>
2020-09-14 14:23:48 -0600

    Bullet

---
+++ Test2/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rBody;
+
+    public float force = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        var forceDirection = new Vector3(force, 0,0);
+        rBody.AddRelativeForce(forceDirection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Test2/Assets/Scripts/Instancer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instancer : MonoBehaviour
+{
+   
+    public GameObject prefab;
+    private void Start()
+    {
+        var location = transform.position;
+        Instantiate(prefab, location, Quaternion.identity);
+       
+    }
+    
+}
