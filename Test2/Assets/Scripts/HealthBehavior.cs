90e332875a5593ce69edea7206f95b311d928467
Riley <rileyharper15@gmail.com>
2020-09-27 17:12:38 -0600

    Forgot to push this yesterday/EnemyScript

---
+++ Test2/Assets/Scripts/HealthBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBehavior : MonoBehaviour
+
+{
+    public int health = 30;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            
+        }
+    }
+}
