ba53c8598e8421ce41bc8f5ff8c4b5ba14bc3fc9
Riley <rileyharper15@gmail.com>
2020-10-07 16:30:52 -0600

    wed

---
+++ Marty Jump/Assets/Scripts/Jump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{ public float jumpSpeed = 8.0F;
+  public float gravity = 20.0F;
+  private Vector3 moveDirection;
+  public CharacterController controller;
+
+  void Start()
+  {
+      
+  }
+  void Update() {
+      if (controller.isGrounded && Input.GetButton("Jump")) {
+          moveDirection.y = jumpSpeed;
+      }
+      moveDirection.y -= gravity * Time.deltaTime;
+      controller.Move(moveDirection * Time.deltaTime);
+  }
+}
---
+++ Test2/Assets/Scripts/Behaviours/StatsBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsBehavior : MonoBehaviour
+{
+    private float defense;
+    private float jumpAmount;
+    private float moveSpeed;
+    private float attack;
+    private float attackSpeed;
+    public Stats stats;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpAmount = stats.jumpHeight;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
