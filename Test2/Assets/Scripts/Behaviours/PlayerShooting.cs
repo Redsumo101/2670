1906419841097f5dfff12fc4cbd1cdf723f60305
Riley <rileyharper15@gmail.com>
2020-10-20 22:54:31 -0600

    Shoot.
    Need to fix the aim.

---
+++ Test2/Assets/Scripts/Behaviours/PlayerShooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int ammoCount = 30;
+    public GameObject prefab;
+    public WaitForSeconds reloadTime;
+    public Transform instancer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reloadTime = new WaitForSeconds(2); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && ammoCount > 0)
+        {
+            StartCoroutine(Fire());
+        }
+    }
+
+    private IEnumerator Fire()
+    {
+        Instantiate(prefab, instancer.position, instancer.rotation);
+        ammoCount--;
+
+        if (ammoCount == 0)
+        {
+            yield return reloadTime;
+            ammoCount = 30;
+        }
+    }
+}
--- Test2/Assets/Scripts/Behaviours/StatsBehavior.cs
+++ Test2/Assets/Scripts/Behaviours/StatsBehavior.cs
@@ -9,8 +9,7 @@ public class StatsBehavior : MonoBehaviour
     private float moveSpeed;
     private float attack;
     private float attackSpeed;
-    private int whichCharacter = 0;
-    [SerializeField] Stats startingStats;
+   [SerializeField] Stats startingStats;
      Stats stat;
     
     // Start is called before the first frame update
@@ -18,8 +17,6 @@ public class StatsBehavior : MonoBehaviour
     {
         stat = startingStats;
         
-        ChangeCharacter();;
-
     }
 
     // Update is called once per frame
@@ -27,47 +24,29 @@ public class StatsBehavior : MonoBehaviour
     {
         ManageStats();
         Debug.Log(jump);
+        Debug.Log(moveSpeed);
        
     }
 
     private void ManageStats()
     {
-        jump = stat.GetStats();
+        
         var nextStats = stat.GetNextStats();
-        int previousCharacter = whichCharacter;
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             stat = nextStats[0];
-            whichCharacter = 0;
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             stat = nextStats[1];
-            whichCharacter = 1;
-           
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             stat = nextStats[2];
-            whichCharacter = 2;
-            
         }
-        if (previousCharacter != whichCharacter)
-        {
-            ChangeCharacter();
-        }
-        
+       
+        jump = stat.GetStats();
+        moveSpeed = stat.GetStats();
     }
-    void ChangeCharacter()
-    {
-        int i = 0;
-        foreach (Transform character in transform)
-        {
-            if(i == whichCharacter)
-                character.gameObject.SetActive(true);
-            else
-                character.gameObject.SetActive(false);
-            i++;
-        }
-    }
+   
 }
