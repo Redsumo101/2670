c3cd0ac74822c2e35d0477879e4349f593bfec92
Riley <rileyharper15@gmail.com>
2020-11-02 15:42:28 -0700

    Follow and Terrain

---
+++ Test2/Assets/Scripts/Behaviours/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private Vector3 cameraOffset;
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+
+    public bool LookAtPLayer = false;
+
+    private void Start()
+    {
+        cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 newPos = PlayerTransform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+        
+        if(LookAtPLayer)
+            transform.LookAt(PlayerTransform);
+    }
+}
