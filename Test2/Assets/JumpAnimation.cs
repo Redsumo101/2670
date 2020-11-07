8ff710c6e63f5fd31da0b40f87a816a259dea2f6
Riley <rileyharper15@gmail.com>
2020-11-06 21:10:50 -0700

    More animation

---
+++ Test2/Assets/JumpAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAnimation : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
