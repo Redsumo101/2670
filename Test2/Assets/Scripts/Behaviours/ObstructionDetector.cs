94b28a9ae6f59009c4398c11d321cfaf5f3a85c4
Riley <rileyharper15@gmail.com>
2020-11-19 13:22:57 -0700

    Still doesnt work. I want to kill myself

---
+++ Test2/Assets/Scripts/Behaviours/ObstructionDetector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstructionDetector : MonoBehaviour
+{
+    public Transform player;
+    private TransparentBehavior obstruction;
+    
+    
+    void Start()
+    {
+        StartCoroutine(DetectObstruction());
+    }
+
+
+
+    IEnumerator DetectObstruction()
+    {
+        while (true)
+        {
+            Vector3 origin = Camera.current.transform.position;
+            Vector3 direction = player.forward;
+            Ray ray = new Ray(origin, direction);
+            yield return new WaitForSeconds(0.5f);
+
+
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, Mathf.Infinity))
+            {
+                obstruction = raycastHit.collider.gameObject.GetComponent<TransparentBehavior>();
+                if (obstruction)
+                {
+                    obstruction.SetTransparent();
+                }
+                else
+                {
+                    obstruction.Normal();
+                }
+            }
+        }
+    }
+}
---
+++ Test2/Assets/Scripts/Behaviours/TransparentBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentBehavior : MonoBehaviour
+{
+   public Material transMat;
+   public Material ogMat;
+   private MeshRenderer mesh;
+
+   private void Start()
+   {
+      mesh = GetComponent<MeshRenderer>();
+      mesh.material = ogMat;
+   }
+
+   public void SetTransparent()
+   {
+      mesh.material = transMat;
+   }
+
+   public void Normal()
+   {
+      mesh.material = ogMat;
+   }
+   
+}
