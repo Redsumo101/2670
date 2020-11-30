26be6b9dc4b431ea46976bd771c57b2f7fd87389
Riley <rileyharper15@gmail.com>
2020-11-29 22:30:24 -0700

    Platforms

---
+++ Test2/Assets/Scripts/Behaviours/Platform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
