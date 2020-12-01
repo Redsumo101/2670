c899a94f303afe5910f45a9f536528a22e9c5a10
Riley <rileyharper15@gmail.com>
2020-11-30 22:40:49 -0700

    A lot

---
+++ Test2/Assets/Scripts/Behaviours/BossSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BossSpawn : MonoBehaviour
+{
+    public List<ButtonPress> buttons;
+
+    void Update()
+    {
+        if (buttons[0].isPressed == true && buttons[1].isPressed == true && buttons[2].isPressed == true)
+        {
+            Debug.Log("Please");
+        }
+    }
+}
---
+++ Test2/Assets/Scripts/Behaviours/ButtonPress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+   public bool isPressed = false;
+   /*public Animator press;*/
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.CompareTag("Player"))
+      {
+         Debug.Log("Pressed");
+         isPressed = true;
+      }
+   }
+}
